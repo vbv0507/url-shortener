60b8a6cc771ffa2697ae18d75d4e61951d1bdde3
vrai48374 <vrai48374@gmail.com>
2026-04-04 14:47:38 +0530

    Refactor index.js and home.ejs for improved structure and styling

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const express=require('express');
-const app=express();
-const port = process.env.PORT || 5001;
-const urlRoute=require('./routes/url.js')
-const connectdb=require('./connection.js');
-const URL=require('./model/url.js');
-const path=require('path');
 require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+const urlRoute = require("./routes/url.js");
+const connectdb = require("./connection.js");
+
 connectdb(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Mongo error", err));
+
 app.use(express.json());
-app.set('view engine','ejs');
-app.set('views',path.resolve('./views'));
-async function renderHome(req,res){
-    const allurls=await URL.find({}).sort({ createdAt: -1 }).lean();
-    return res.render('home',{
-        urls:allurls
-    });
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+function renderHome(req, res) {
+  return res.render("home");
 }
-app.get('/',renderHome);
-app.get('/test',renderHome);
-app.use('/url',urlRoute);
-app.listen(port,()=>{console.log(`server start at port ${port}\n`)})
+
+app.get("/", renderHome);
+app.get("/test", renderHome);
+app.use("/url", urlRoute);
+
+app.listen(port, () => {
+  console.log(`server start at port ${port}\n`);
+});
