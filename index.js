b40fd0a705e097cd0edb9dbeaf8280e83f830e46
vrai48374 <vrai48374@gmail.com>
2026-04-29 23:38:51 +0530

    Refactor code formatting and improve readability in index.js; add redirect URL functionality

--- index.js
+++ index.js
@@ -3,41 +3,50 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const URL = require("./model/url.js");
-const cookieParser = require('cookie-parser');
-const { restrictToLoggedinUserOnly } = require('./middleware/auth.middleware.js');
+const cookieParser = require("cookie-parser");
+const { restrictToLoggedinUserOnly } = require("./middleware/auth.middleware.js");
 
 const app = express();
 const port = process.env.PORT || 5001;
 
 const urlRoute = require("./routes/url.js");
 const connectdb = require("./connection.js");
-const useroute = require('./routes/user.js');
+const useroute = require("./routes/user.js");
 
+//  NEW IMPORT
+const { redirecturl } = require("./controller/url");
+
+// DB
 connectdb(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("Mongo error", err));
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.resolve("./public")));
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+// Home
 async function renderHome(req, res) {
   const allurls = await URL.find({}).sort({ createdAt: -1 }).lean();
-  return res.render("home", {
-    urls: allurls,
-  });
+  return res.render("home", { urls: allurls });
 }
 
-
+// Routes
 app.use("/api/url", urlRoute);
 app.use("/user", useroute);
 
 app.get("/", renderHome);
 app.get("/home", restrictToLoggedinUserOnly, renderHome);
 
+//  IMPORTANT (LAST LINE ROUTE)
+app.get("/:shortId", redirecturl);
+
+// Start
 app.listen(port, () => {
-  console.log(`server start at port ${port}\n`);
+  console.log(`Server started at port ${port}`);
 });
