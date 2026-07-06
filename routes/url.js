d48b1a2084c3be9e8b0052629e7f4f3f988d22a9
vrai48374 <vbv0507@users.noreply.github.com>
2026-07-06 22:41:12 +0530

    for developer i change the routes from restirctonlylogin to verifyApiKey

--- routes/url.js
+++ routes/url.js
@@ -7,12 +7,12 @@ const verifyApiKey=require('../middleware/apiKey.middleware.js');
 const { start } = require("../controller/client.js");
 
 router.post('/start',start);
-router.post('/', restrictToLoggedinUserOnly, createUrlLimiter, generateNewshortUrl);
+router.post('/', verifyApiKey, createUrlLimiter, generateNewshortUrl);
 router.post('/api', verifyApiKey, createUrlLimiter, generateNewshortUrl);
-router.get('/my-links', restrictToLoggedinUserOnly, getMyLinks);
-router.get('/qr/:shortId',restrictToLoggedinUserOnly,getQrCode);
-router.get('/analytics/:shortid',restrictToLoggedinUserOnly, analyticsLimiter, getanalytics);
-router.delete("/:shortId", restrictToLoggedinUserOnly, deletelink);
+router.get('/my-links', verifyApiKey, getMyLinks);
+router.get('/qr/:shortId',verifyApiKey,getQrCode);
+router.get('/analytics/:shortid',verifyApiKey, analyticsLimiter, getanalytics);
+router.delete("/:shortId", verifyApiKey, deletelink);
 
 router.post('/expand', analyticsLimiter, urlexpand);
 
